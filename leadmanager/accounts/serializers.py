29db75a93ca0b3411b40bcb109f52aa7ed40c98e
ThomasChin <tchin10@outlook.com>
2019-05-06 15:55:09 -0700

    Implement django token authentication

--- leadmanager/accounts/serializers.py
+++ leadmanager/accounts/serializers.py
@@ -18,3 +18,16 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        # is_active is a property of Django Users
+        if user and user.is_active: 
+            return user
+        raise serializers.ValidationError("Incorrect Credentials")
+        
