040c75a09206f3b794fb221d23627d25ed0f53e4
ThomasChin <tchin10@outlook.com>
2019-05-08 21:34:36 -0700

    Add register/login components

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -3,8 +3,9 @@ import leads from './leads';
 import errors from './errors';
 import messages from './messages';
 
+
 export default combineReducers({
   leads,
   errors,
-  messages
+  messages,
 });
